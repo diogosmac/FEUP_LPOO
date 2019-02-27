9657d08ef76e9083454a16357d68ac474e9c906f
diogosmac <diogo.smac@gmail.com>
2019-02-27 17:42:23 +0000

    Game class added.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -9,26 +9,8 @@ import java.io.IOException;
 public class Application {
 
     public static void main(String[] args) {
-
-        try {
-
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-
-            screen.clear();
-            screen.setCharacter(10, 10, new TextCharacter('X'));
-            screen.refresh();
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-
-        }
-
+        Game game = new Game();
+        game.run();
     }
 
 }
