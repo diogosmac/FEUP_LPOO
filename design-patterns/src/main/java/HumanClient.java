070ee9f8e6e9214fa7c673534191d0ee0c53f32e
diogosmac <diogo.smac@gmail.com>
2019-03-26 09:22:16 +0000

    Design Patterns - Chapter Three: Observer Pattern

---
+++ design-patterns/src/main/java/HumanClient.java
@@ -0,0 +1,15 @@
+public class HumanClient implements Client {
+    public HumanClient() { }
+
+    public void happyHourStarted(Bar bar) {
+
+    }
+
+    public void happyHourEnded(Bar bar) {
+
+    }
+
+    public void wants(StringRecipe recipe, StringBar bar) {
+
+    }
+}
